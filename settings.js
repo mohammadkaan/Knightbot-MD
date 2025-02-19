e77692b172cf5fdec53f39ae128f6c1e1a334790
mohammadkaan <146885545+mohammadkaan@users.noreply.github.com>
2025-02-19 21:51:15 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
+  packname: 'SLM',
+  author: `𝐼'𝓂 𝒯𝒽𝑒 𝒢𝒽𝑜𝓈𝓉 𝒪𝒻_ ˢᴸᴹ_`,
+  botName: "SLM||Prime",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '947768967587', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
